62d4ebf7641bf28c9c2d4475f753fc48c59696ff
Abdennour Hg <abdennourhg2003@gmail.com>
2024-12-20 13:38:35 +0100

    exo 5

---
+++ exo5/main.js
@@ -0,0 +1,76 @@
+import {quotes} from './quotes.js'
+
+const quotes2 = document.querySelector(".quotes2");
+
+
+quotes.forEach(quote => {
+    const card = document.createElement("div");    
+    card.classList.add("card");
+    quotes2.appendChild(card);
+
+    const citations = document.createElement("span"); 
+    citations.classList.add("quote");
+    citations.innerHTML = `"${quote["quote"]}"`;
+    card.appendChild(citations);
+
+
+    const br = document.createElement("br");
+    card.appendChild(br);
+
+
+    const author = document.createElement("span"); 
+    author.classList.add("auteur");
+    author.innerHTML = quote["auteur"];
+    card.appendChild(author);
+
+    const fav = document.createElement("i"); 
+    fav.classList.toggle("fa-regular");
+    fav.classList.toggle("fa-heart");
+    card.appendChild(fav);
+    
+    fav.addEventListener("click", function() {
+        const author = quote["auteur"]; // Récupérer l'auteur
+        const citations = `"${quote["quote"]}"`; // Récupérer quote
+
+        const isFavorite = fav.classList.contains("fas"); // Vérifie si l'élément est en favoris
+
+    if (isFavorite) {
+        // Si déjà en favoris, on supprime
+        localStorage.removeItem(author); // Supprime seulement cet auteur
+        localStorage.removeItem(citations); // Supprime seulement cet auteur
+        fav.classList.add("fa-heart");
+        fav.classList.toggle("fas");
+        fav.classList.add("fav");
+    } else {
+        // Sinon, on ajoute en favoris
+        localStorage.setItem(author, "auteur"); // Ajoute l'auteur au localStorage
+        localStorage.setItem(citations, "quote"); // Ajoute l'auteur au localStorage
+        fav.classList.toggle("fas");
+        fav.classList.remove("fav");
+    }
+
+    });
+    console.log(localStorage)
+    
+    // // Create a "Favorite" button
+    // const favButton = document.createElement("button");
+    // favButton.classList.add("fav-btn");
+    // favButton.innerText = "Add to Favorites";
+    // card.appendChild(favButton);
+
+    // // Add event listener to toggle favorite status
+    // favButton.addEventListener("click", function() {
+    //     if (favButton.innerText === "Add to Favorites") {
+    //         favButton.innerText = "Remove from Favorites";
+    //         favButton.classList.add("fav"); // Add "fav" class for styling
+    //     } else {
+    //         favButton.innerText = "Add to Favorites";
+    //         favButton.classList.remove("fav"); // Remove "fav" class for styling
+    //     }
+    // });
+
+
+
+
+});
+
---
+++ exo5/quotes.js
@@ -0,0 +1,62 @@
+export var quotes = [{
+    id: 1,
+    auteur: "Serge Karamazov",
+    title: "Serge Karamazov",
+    quote: "Barrez vous **** de mimes."
+},
+{
+    id: 2,
+    auteur: "Georges Abitbol",
+    title: "Georges Abitbol",
+    quote: "Le train de tes injures roule sur le rail de mon indifférence."
+},
+{
+    id: 3,
+    auteur: "Hubert Bonisseur de la Bath",
+    title: "Hubert Bonisseur de la Bath",
+    quote: "Ça ne prenait pas beaucoup plus de temps.",
+},
+{
+    id: 4,
+    auteur: "Moulinier",
+    title: "Moulinier",
+    quote: "L'habit ne fait pas le moine, mais il fait l'agent... Même non titularisé.",
+ },
+ {
+    id: 5,
+    auteur: "Orson Welles",
+    title: "Orson Welles",
+    quote: "C'est du vol et du plagiat. J'aime pas trop les voleurs et..."
+}
+];
+
+// {
+//     "id": 1,
+//     "author": "Serge Karamazov",
+//     "title": "Serge Karamazov",
+//     "content": "Barrez vous **** de mimes."
+// },
+// {
+//     "id": 2,
+//     "author": "Georges Abitbol",
+//     "title": "Georges Abitbol",
+//     "content": "Le train de tes injures roule sur le rail de mon indifférence."
+// },
+// {
+//     "id": 3,
+//     "author": "Hubert Bonisseur de la Bath",
+//     "title": "Hubert Bonisseur de la Bath",
+//     "content": "Ça ne prenait pas beaucoup plus de temps.",
+// },
+// {
+//     "id": 4,
+//     "author": "Moulinier",
+//     "title": "Moulinier",
+//     "content": "L'habit ne fait pas le moine, mais il fait l'agent... Même non titularisé.",
+//  };
+//  {
+//     "id": 5,
+//     "author": "Orson Welles",
+//     "title": "Orson Welles",
+//     "content": "C'est du vol et du plagiat. J'aime pas trop les voleurs et..."
+// }
