65e3882196dcd4b6fae86a467d2de57622b5feb9
Abdennour Hg <abdennourhg2003@gmail.com>
2025-01-04 23:43:25 +0100

    update

--- exo 4/main.js
+++ exo 4/main.js
@@ -1,67 +1,93 @@
 const carres = document.querySelectorAll(".carre");
-const float = document.querySelector(".float");
+// const float = document.querySelector(".float");
+const socialNames = {
+    'fa-facebook': 'Facebook',
+    'fa-twitter': 'Twitter',
+    'fa-instagram': 'Instagram'
+};
+
+// function updateTextBasedOnCarre(carre) {
 
 
-function updateTextBasedOnCarre(carre) {
+//     // Determine which carre was clicked and update text accordingly
+//     if (carre.classList.contains("blue")) {
+//         socialName.innerHTML = "Facebook";  
+//     } else if (carre.classList.contains("blueciel")) {
+//         socialName1.innerHTML = "Twitter";   
+//     } else if (carre.classList.contains("purple")) {
+//         socialName2.innerHTML = "Instagram"; 
+//     }
 
-
-    // Determine which carre was clicked and update text accordingly
-    if (carre.classList.contains("blue")) {
-        socialName.innerHTML = "Facebook";  
-    } else if (carre.classList.contains("blueciel")) {
-        socialName1.innerHTML = "Twitter";   
-    } else if (carre.classList.contains("purple")) {
-        socialName2.innerHTML = "Instagram"; 
-    }
-
- }
+//  }
 
 
 
 carres.forEach(carre=>{
     carre.addEventListener("click", function (){
 
+        const icon = carre.querySelector('i');
+        const socialName = socialNames[icon.classList[1]];
+
+        // Reset all social names
+        document.querySelectorAll('.carre p').forEach(p => p.innerHTML = '');
+
+        // Set the clicked social name
+        // carre.querySelector('p').innerHTML = socialName;
+
+        // Toggle the class
+        carre.classList.toggle('carreN');
+        // If the class is added, set the social name, otherwise leave it empty
+        if (carre.classList.contains('carreN')) {
+            carre.querySelector('p').innerHTML = socialName;
+        }
+
+        // Update the float background color
         const carresStyle = window.getComputedStyle(carre);
         const carresBackground = carresStyle.getPropertyValue("background-color");
+        const float = document.querySelector('.float');
+        float.style.backgroundColor = carre.classList.contains('carreN') ? carresBackground : '';
 
-        carre.classList.toggle("carreN");
+    //     const carresStyle = window.getComputedStyle(carre);
+    //     const carresBackground = carresStyle.getPropertyValue("background-color");
+
+    //     carre.classList.toggle("carreN");
        
-        float.style.backgroundColor = carresBackground;
+    //     float.style.backgroundColor = carresBackground;
         
         
 
-        if (carre.classList.contains("carreN")) {
-            float.style.backgroundColor = carresBackground;
+    //     if (carre.classList.contains("carreN")) {
+    //         float.style.backgroundColor = carresBackground;
 
-        }
-     else {
-        // If 'carreN' is removed, reset background color
-        float.style.backgroundColor = "";
-    }
-        if (carre.classList.contains("carreN")) {
-            socialName.innerHTML = "Facebook";
+    //     }
+    //  else {
+    //     // If 'carreN' is removed, reset background color
+    //     float.style.backgroundColor = "";
+    // }
+    //     if (carre.classList.contains("carreN")) {
+    //         socialName.innerHTML = "Facebook";
 
-        }
-     else {
+    //     }
+    //  else {
      
-        socialName.innerHTML = "";
-    }
-        if (carre.classList.contains("carreN")) {
-            socialName1.innerHTML = "Twitter";
+    //     socialName.innerHTML = "";
+    // }
+    //     if (carre.classList.contains("carreN")) {
+    //         socialName1.innerHTML = "Twitter";
 
-        }
-     else {
+    //     }
+    //  else {
         
-        socialName1.innerHTML = "";
-    }
-        if (carre.classList.contains("carreN")) {
-            socialName2.innerHTML = "Instagram";
+    //     socialName1.innerHTML = "";
+    // }
+    //     if (carre.classList.contains("carreN")) {
+    //         socialName2.innerHTML = "Instagram";
 
-        }
-     else {
+    //     }
+    //  else {
         
-        socialName2.innerHTML = "";
-    }
+    //     socialName2.innerHTML = "";
+    // }
 
     
 
