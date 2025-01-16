038775a4a926994e9e586657ad0db637f0ce8be5
Abdennour Hg <abdennourhg2003@gmail.com>
2025-01-16 23:51:17 +0100

    update

--- exo5/main.js
+++ exo5/main.js
@@ -57,6 +57,7 @@ quotes.forEach(quote => {
     console.log(localStorage)
     console.log(localStorage)
     console.log(localStorage)
+    console.log(localStorage)
     
     // // Create a "Favorite" button
     // const favButton = document.createElement("button");
